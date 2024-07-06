2ae3fdbfb8a6ed9a03aba39ef311774785f2fe44
Elias Wojahn <elias.wojahn@bridge-mt.com>
2024-07-06 10:11:04 -0300

    Update login api url

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -13,7 +13,7 @@ export default function Login() {
 
     async function verificaLogin(data) {
 
-        const response = await fetch('http://localhost:3004/login',
+        const response = await fetch('https://veraflor.onrender.com/login',
             {
                 method: "POST",
                 headers: { "Content-type": "application/json" },
