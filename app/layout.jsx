23062525c8622f4f22242626a5e03bc57492ba36
Elias Wojahn <elias.wojahn@bridge-mt.com>
2024-09-01 11:45:17 -0300

    Add tags to improve SEO postion

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,8 +7,8 @@ import AdministradorProvider from "@/contexts/administrator";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Veraflor Garden",
-  description: "Floricultura Veraflor Garden",
+  title: "Veraflor Garden - Floricultura e Jardinagem",
+  description: "Explore a variedade de plantas, flores e acessórios de jardinagem da Veraflor Garden.",
 };
 
 export default function RootLayout({ children }) {
@@ -16,6 +16,42 @@ export default function RootLayout({ children }) {
     <html lang="pt-br">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://veraflor.vercel.app" />
+        <meta property="og:image" content="/images/pelotas.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content="/images/pelotas.png" />
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Florist",
+              "name": "Veraflor Garden",
+              "description": "Explore a variedade de plantas, flores e acessórios de jardinagem da Veraflor Garden.",
+              "image": "https://veraflor.vercel.app/images/pelotas.png",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Av. Dr. Félix Antônio Caputo, 251",
+                "addressLocality": "Pelotas",
+                "addressRegion": "RS",
+                "postalCode": "96070-060",
+                "addressCountry": "BR"
+              },
+              "telephone": "+55-(53)99956-1458",
+              "sameAs": [
+                "https://www.facebook.com/veraflorgarden",
+                "https://www.instagram.com/veraflorgarden"
+              ],
+              "url": "https://veraflor.vercel.app"
+            }
+          `}
+        </script>
       </head>
       <body className={`${inter.className} layout`}>
         <AdministradorProvider>
