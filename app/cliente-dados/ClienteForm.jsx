872e918c8c9c888ffa864d8f501782ee2e0aa7f7
Elias Wojahn <elias.wojahn@bol.com.br>
2024-11-10 19:59:57 -0300

    Block cpf edit

--- app/cliente-dados/ClienteForm.jsx
+++ app/cliente-dados/ClienteForm.jsx
@@ -13,57 +13,58 @@ const ClienteForm = ({ cliente, setCliente, onSubmit }) => {
             <form onSubmit={onSubmit} className={styles.form}>
                 <label className={styles.label}>
                     Nome completo
-                    <input 
-                        type="text" 
-                        name="nome" 
-                        value={cliente.nome} 
-                        onChange={handleInputChange} 
-                        className={styles.input} 
+                    <input
+                        type="text"
+                        name="nome"
+                        value={cliente.nome}
+                        onChange={handleInputChange}
+                        className={styles.input}
                     />
                 </label>
 
                 <label className={styles.label}>
                     Data de nascimento
-                    <input 
-                        type="date" 
-                        name="dataNasc" 
-                        value={cliente.dataNasc} 
-                        onChange={handleInputChange} 
-                        className={styles.input} 
+                    <input
+                        type="date"
+                        name="dataNasc"
+                        value={cliente.dataNasc}
+                        onChange={handleInputChange}
+                        className={styles.input}
                     />
                 </label>
 
                 <label className={styles.label}>
                     CPF
-                    <input 
-                        type="text" 
-                        name="cpf" 
-                        value={cliente.cpf} 
-                        onChange={handleInputChange} 
-                        className={styles.input} 
+                    <input
+                        type="text"
+                        name="cpf"
+                        value={cliente.cpf}
+                        onChange={handleInputChange}
+                        className={styles.input}
+                        disabled={cliente.cpf !== ''} 
                     />
                 </label>
 
                 <label className={styles.label}>
                     E-mail
-                    <input 
-                        type="email" 
-                        name="email" 
-                        value={cliente.email} 
-                        onChange={handleInputChange} 
-                        className={styles.input} 
+                    <input
+                        type="email"
+                        name="email"
+                        value={cliente.email}
+                        onChange={handleInputChange}
+                        className={styles.input}
                     />
                 </label>
 
                 <div className={styles.phoneGroup}>
                     <label className={`${styles.label} ${styles.dddLabel}`}>
                         DDD
-                        <input 
-                            type="text" 
-                            name="ddd" 
-                            value={cliente.ddd || ''} 
-                            onChange={handleInputChange} 
-                            className={`${styles.input} ${styles.dddInput}`} 
+                        <input
+                            type="text"
+                            name="ddd"
+                            value={cliente.ddd || ''}
+                            onChange={handleInputChange}
+                            className={`${styles.input} ${styles.dddInput}`}
                             maxLength="2"
                             inputMode="numeric"
                             pattern="[0-9]*"
@@ -71,12 +72,12 @@ const ClienteForm = ({ cliente, setCliente, onSubmit }) => {
                     </label>
                     <label className={`${styles.label} ${styles.celularLabel}`}>
                         Telefone Celular
-                        <input 
-                            type="text" 
-                            name="celular" 
-                            value={cliente.celular || ''} 
-                            onChange={handleInputChange} 
-                            className={`${styles.input} ${styles.celularInput}`} 
+                        <input
+                            type="text"
+                            name="celular"
+                            value={cliente.celular || ''}
+                            onChange={handleInputChange}
+                            className={`${styles.input} ${styles.celularInput}`}
                             maxLength="9"
                             inputMode="numeric"
                             pattern="[0-9]*"
